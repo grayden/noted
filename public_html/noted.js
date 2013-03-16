2c9a05232b7823e788f42de4de19851491ec3945
Grayden Smith <grayden@tech-vision.ca>
2013-03-16 07:30:04 -0400

    Setup specrunner and app file
    Create a noted.js file to hold configuration and shared functions
    
    Move jasmine into the library and create a spec runner outline to run tests for the application

---
+++ public_html/noted.js
@@ -0,0 +1,6 @@
+var Noted = {
+    Config: {
+        CollectionsSource: "http://127.0.0.1:5984/noted/_design/collections/_view/collections",
+        TagsSource: "http://127.0.0.1:5984/noted/_design/tags/_view/tags"
+    }
+};
