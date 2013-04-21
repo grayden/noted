deb73fc45e2a0824f7e9f0dc911fc04fe66db295
Grayden Smith <grayden@tech-vision.ca>
2013-04-21 06:43:56 -0400

    Add layout for query builder and result selector
    Create the query builder template. Create radio buttons underneath to select result type. Add the search executor button.

--- public_html/src/SearchToolView.js
+++ public_html/src/SearchToolView.js
@@ -1,7 +1,13 @@
 Noted.SearchToolView = Backbone.View.extend({
     id: 'search-tool-container',
+    template: '#search-tool-template',
+    
+    initialize: function () {
+        this.template = _.template($(this.template).html());
+    },
     
     render: function () {
+        this.$el.html(this.template());
         return this;
     }
 });
